727c8cdc7642ba989a8eeb690d9d646a2ffe7f90
0xYami <0xYami@proton.me>
2023-03-27 01:23:53 +0200

    update styles + add basic query

--- packages/ui/src/app.tsx
+++ packages/ui/src/app.tsx
@@ -5,7 +5,7 @@ import { Onboarding } from './pages/onboarding';
 
 const App: Component = () => {
   return (
-    <div class="w-[360px] h-[540px] bg-blue-200">
+    <div class="w-[360px] h-[540px] text-white bg-black border border-zinc-700">
       <Routes>
         <Route path="/" element={<Navigate href="/index.html" />} />
         <Route path="/index.html" component={Home} />
--- packages/ui/src/pages/home.tsx
+++ packages/ui/src/pages/home.tsx
@@ -1,11 +1,34 @@
-import type { Component } from 'solid-js';
-import { Link } from '../components/link';
+import { createQuery } from '@tanstack/solid-query';
+import { Component, For, Match, Switch } from 'solid-js';
 
 export const Home: Component = () => {
+  const user = createQuery({
+    queryKey: () => ['address'],
+    queryFn: async () => {
+      const address = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045';
+      const res = await fetch(`http://localhost:4000/address/${address}`);
+      if (!res.ok) throw new Error('Failed to retrieve address information');
+      const data = await res.json();
+      return data;
+    },
+  });
+
   return (
     <div>
       <h1>Home page</h1>
-      <Link path="/onboarding">Go to onboarding</Link>
+      <Switch>
+        <Match when={user.isLoading}>
+          <p>Loading...</p>
+        </Match>
+        <Match when={user.isError}>
+          <p>Error occured</p>
+        </Match>
+        <Match when={user.isSuccess}>
+          <For each={user.data}>
+            {(token) => <p>{token.name}</p>}
+          </For>
+        </Match>
+      </Switch>
     </div>
   );
 };
