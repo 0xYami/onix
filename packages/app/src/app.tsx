2015433ce8ec3c1a41b161336aec66b1091d0bab
0xYami <0xYami@proton.me>
2023-03-26 17:31:09 +0200

    feat: Vercel preview
    - rename `App.*` -> `app.*`
    - setup TailwindCSS
    - clean-up + set layout

---
+++ packages/app/src/app.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="w-[360px] h-[540px] bg-blue-200">
+      <h1>Home page</h1>
+    </div>
+  );
+};
+
+export default App;
