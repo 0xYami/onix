42b0fd10c4d2ff16b5038dec4002f8b306b4f0a5
0xYami <0xYami@proton.me>
2023-03-27 12:44:05 +0200

    replace `Koa` with `Fastify`

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,9 @@
-import Koa from 'koa';
-import Router from '@koa/router';
+import Fastify from 'fastify'
 import { Network, Alchemy } from 'alchemy-sdk';
 
-const app = new Koa();
-const router = new Router();
+const router = Fastify({
+  logger: true
+});
 
 const settings = {
   apiKey: process.env.ALCHEMY_API_KEY,
@@ -13,14 +13,17 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 router
-  .get('/_health', async (ctx) => {
-    ctx.body = 'healthy';
+  .get('/_health', () => {
+    return 'healthy'
   })
-  .get('/', async (ctx) => {
+  .get('/', async () => {
     const latestBlock = await alchemy.core.getBlockNumber();
-    ctx.body = latestBlock;
+    return { result: latestBlock };
   });
 
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000);
+try {
+  await router.listen({ port: 4000 });
+} catch (err) {
+  router.log.error(err);
+  process.exit(1);
+}
