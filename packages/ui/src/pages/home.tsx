690b44ddf6f48042addaa7db677198248ccfaf7a
0xYami <0xYami@proton.me>
2023-03-27 15:21:05 +0200

    setup http client

--- packages/ui/src/pages/home.tsx
+++ packages/ui/src/pages/home.tsx
@@ -1,34 +1,23 @@
+import { Component, For, Suspense } from 'solid-js';
 import { createQuery } from '@tanstack/solid-query';
-import { Component, For, Match, Switch } from 'solid-js';
+import { addressDetailsSchema } from '@onix/schemas';
+import { request } from '../lib/api';
 
 export const Home: Component = () => {
   const user = createQuery({
     queryKey: () => ['address'],
     queryFn: async () => {
       const address = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045';
-      const res = await fetch(`http://localhost:4000/address/${address}`);
-      if (!res.ok) throw new Error('Failed to retrieve address information');
-      const data = await res.json();
-      return data;
+      return request({ url: `/address/${address}`, schema: addressDetailsSchema });
     },
   });
 
   return (
     <div>
       <h1>Home page</h1>
-      <Switch>
-        <Match when={user.isLoading}>
-          <p>Loading...</p>
-        </Match>
-        <Match when={user.isError}>
-          <p>Error occured</p>
-        </Match>
-        <Match when={user.isSuccess}>
-          <For each={user.data}>
-            {(token) => <p>{token.name}</p>}
-          </For>
-        </Match>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <For each={user.data?.assets}>{(asset) => asset.name}</For>
+      </Suspense>
     </div>
   );
 };
