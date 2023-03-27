9e91ad486ac776f80757b789623a4f7b3b4a149b
0xYami <0xYami@proton.me>
2023-03-27 16:05:15 +0200

    add `ErrorBoundary`

--- packages/ui/src/app.tsx
+++ packages/ui/src/app.tsx
@@ -1,17 +1,21 @@
-import type { Component } from 'solid-js';
+import { Component, ErrorBoundary } from 'solid-js';
 import { Routes, Route, Navigate } from '@solidjs/router';
 import { Home } from './pages/home';
 import { Onboarding } from './pages/onboarding';
+import { Header } from './components/header';
 
 const App: Component = () => {
   return (
-    <div class="w-[360px] h-[540px] text-white bg-black border border-zinc-700">
-      <Routes>
-        <Route path="/" element={<Navigate href="/index.html" />} />
-        <Route path="/index.html" component={Home} />
-        <Route path="/index.html/onboarding" component={Onboarding} />
-      </Routes>
-    </div>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <div class="w-[360px] h-[540px] text-white bg-black border border-zinc-700">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate href="/index.html" />} />
+          <Route path="/index.html" component={Home} />
+          <Route path="/index.html/onboarding" component={Onboarding} />
+        </Routes>
+      </div>
+    </ErrorBoundary>
   );
 };
 
