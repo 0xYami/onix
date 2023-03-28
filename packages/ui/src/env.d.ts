4308833e3b7d7188412c70291cbeeb46380772fc
0xYami <0xYami@proton.me>
2023-03-29 01:17:26 +0200

    fix lint

--- packages/ui/src/env.d.ts
+++ packages/ui/src/env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface ImportMetaEnv {
   readonly VITE_SERVER_HOST_URL?: string | undefined;
 }
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
