c84d9202b986c335aa79d4c44786450725240eb6
0xYami <118700396+0xYami@users.noreply.github.com>
2023-04-04 18:08:49 +0200

    [ui] Improve user experience when copying address (#100)

--- packages/ui/src/components/header.tsx
+++ packages/ui/src/components/header.tsx
@@ -1,10 +1,26 @@
-import type { Component } from 'solid-js';
+import { createEffect, createSignal, Show, type Component } from 'solid-js';
 import { userStore } from '../store';
 import { copyToClipboard, truncateMiddle } from '../lib/utils';
 import { GasPumpIcon } from './icons/gas-pump';
 import { CopyIcon } from './icons/copy';
+import { CheckIcon } from './icons/check';
 
 export const Header: Component = () => {
+  const [copying, setCopying] = createSignal(false);
+
+  createEffect(() => {
+    if (!copying()) return;
+    setTimeout(() => {
+      setCopying(false);
+    }, 2000);
+  });
+
+  const copyAddress = () => {
+    if (!userStore.address) return;
+    setCopying(true);
+    copyToClipboard(userStore.address);
+  };
+
   return (
     <header class="relative h-[48px] flex items-center px-4 space-x-2 border-b-[0.3px] border-zinc-700 text-xs">
       <div class="flex items-center space-x-1">
@@ -16,12 +32,25 @@ export const Header: Component = () => {
         <span>$2.01</span>
       </div>
       <div class="absolute right-0 pr-4 flex items-center space-x-1">
-        {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-        <button type="button" onClick={() => copyToClipboard(userStore.address!)}>
-          <CopyIcon class="w-[14px] h-[14px]" />
-        </button>
-        {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-        <span>{truncateMiddle(userStore.address!, 11)}</span>
+        <Show
+          when={!copying()}
+          fallback={
+            <div class="flex items-center space-x-2 text-green-600">
+              <CheckIcon />
+              <span>Copied</span>
+            </div>
+          }
+        >
+          <button
+            type="button"
+            onClick={copyAddress}
+            class="flex items-center space-x-2 text-zinc-400/90 hover:text-white duration-200"
+          >
+            <CopyIcon class="w-[14px] h-[14px]" />
+            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+            <span>{truncateMiddle(userStore.address!, 11)}</span>
+          </button>
+        </Show>
       </div>
     </header>
   );
