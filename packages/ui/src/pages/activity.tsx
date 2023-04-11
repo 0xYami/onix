d45be723544f66cf33b7102b4833f449a34dab9a
0xYami <118700396+0xYami@users.noreply.github.com>
2023-04-11 21:00:40 +0200

    add activity page (#122)

---
+++ packages/ui/src/pages/activity.tsx
@@ -0,0 +1,99 @@
+import { For, Match, Suspense, Switch, type Component } from 'solid-js';
+import { createQuery } from '@tanstack/solid-query';
+import { alchemyAssetTransfersSchema, type AlchemyAssetTransfer } from '@onix/schemas';
+import { userStore } from '../store';
+import { httpClient } from '../lib/http';
+import { truncateMiddle } from '../lib/utils';
+import { MainLayout } from '../layouts/main';
+import { SendIcon } from '../components/icons/send';
+import { ReceiveIcon } from '../components/icons/receive';
+
+export const Activity: Component = () => {
+  const { currentAccount } = userStore;
+
+  const activityQuery = createQuery({
+    queryKey: () => ['activity', currentAccount?.address],
+    queryFn: async () => {
+      return httpClient.get({
+        url: `/users/${currentAccount?.address}/activity`,
+        validation: {
+          response: alchemyAssetTransfersSchema.transform(({ result }) => {
+            return result.transfers.reduce((acc, curr) => {
+              const date = new Date(curr.metadata.blockTimestamp).toDateString();
+              if (acc[date]) {
+                acc[date].push(curr);
+              } else {
+                acc[date] = [curr];
+              }
+              return acc;
+            }, {} as Record<string, AlchemyAssetTransfer[]>);
+          }),
+        },
+      });
+    },
+    enabled: !!currentAccount?.address,
+  });
+
+  return (
+    <MainLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div class="p-3 text-xl font-bold">Activity</div>
+        <ul class="max-h-96 p-3 space-y-5 overflow-y-scroll">
+          <For each={Object.entries(activityQuery.data || [])}>
+            {([date, transfer]) => (
+              <div class="space-y-3">
+                <div>
+                  {new Date(date).toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric',
+                  })}
+                </div>
+                <ul class="space-y-1">
+                  <For each={transfer}>
+                    {(transfer) => {
+                      const isSender =
+                        transfer.from.toLowerCase() === currentAccount?.address.toLowerCase();
+                      const linkToEtherscan = `https://etherscan.io/tx/${transfer.hash}`;
+                      return (
+                        <li class="flex items-center justify-between">
+                          <a
+                            href={linkToEtherscan}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="w-full flex space-x-2 p-2 rounded-lg hover:bg-zinc-700/50"
+                          >
+                            <Switch>
+                              <Match when={isSender}>
+                                <SendIcon class="w-10 h-10 p-3 rounded-full bg-zinc-700/60" />
+                                <div class="flex flex-col">
+                                  <span>Sent</span>
+                                  <span class="text-sm text-zinc-400">
+                                    To: {transfer.to ? truncateMiddle(transfer.to, 11) : null}
+                                  </span>
+                                </div>
+                              </Match>
+                              <Match when={!isSender}>
+                                <ReceiveIcon class="w-10 h-10 p-3 rounded-full bg-zinc-700/60" />
+                                <div class="flex flex-col">
+                                  <span>Received</span>
+                                  <span class="text-sm text-zinc-400">
+                                    From: {transfer.from ? truncateMiddle(transfer.from, 11) : null}
+                                  </span>
+                                </div>
+                              </Match>
+                            </Switch>
+                          </a>
+                        </li>
+                      );
+                    }}
+                  </For>
+                </ul>
+              </div>
+            )}
+          </For>
+        </ul>
+      </Suspense>
+    </MainLayout>
+  );
+};
