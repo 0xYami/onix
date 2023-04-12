dd3e8232ae55874faebedc692f8ed6c99f274da6
0xYami <118700396+0xYami@users.noreply.github.com>
2023-04-12 20:33:42 +0200

    ui(settings): Allow to export private key (#131)
    * ui(settings): Allow to export private key
    
    * add copy animation

--- packages/ui/src/pages/settings/change-password.tsx
+++ packages/ui/src/pages/settings/change-password.tsx
@@ -1,7 +1,8 @@
-import { createMemo, createSignal, Match, Show, Switch, type Component } from 'solid-js';
+import { createSignal, Match, Switch, type Component } from 'solid-js';
 import { useNavigate } from '@solidjs/router';
 import { userStore } from '../../store';
 import { storage } from '../../lib/storage';
+import { AuthStep } from './index';
 import { ChevronLeftIcon } from '../../components/icons/chevron-left';
 import { EyeIcon } from '../../components/icons/eye';
 import { EyeSlashIcon } from '../../components/icons/eye-slash';
@@ -32,65 +33,6 @@ export const ChangePassword: Component = () => {
         </Match>
       </Switch>
     </div>
-  );
-};
-
-const AuthStep: Component<{ onNext: () => void }> = (props) => {
-  const [password, setPassword] = createSignal('');
-  const [showPassword, setShowPassword] = createSignal(false);
-  const [hasError, setHasError] = createSignal(false);
-  const isValid = createMemo(() => userStore.password === password());
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        props.onNext();
-      }}
-      class="relative h-full flex flex-col mb-4"
-    >
-      <label for="password" class="uppercase mt-4 mb-2">
-        enter new password
-      </label>
-      <div class="relative flex items-center">
-        <input
-          id="password"
-          type={showPassword() ? 'text' : 'password'}
-          value={password()}
-          onInput={(event) => setPassword(event.currentTarget.value)}
-          required
-          autofocus
-          onFocus={() => setHasError(false)}
-          onBlur={() => {
-            if (isValid()) return;
-            setHasError(true);
-          }}
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          pattern={userStore.password!}
-          title="Doesn't match current password"
-          placeholder="Password"
-          class="w-full bg-black border-[] border-zinc-700 rounded"
-        />
-        <button
-          type="button"
-          onClick={() => setShowPassword((prev) => !prev)}
-          class="absolute right-2 z-10"
-        >
-          {showPassword() ? <EyeSlashIcon /> : <EyeIcon />}
-        </button>
-      </div>
-      <Show when={hasError()}>
-        <span class="mt-2 text-xs text-red-500">Doesn't match current password</span>
-      </Show>
-      <button
-        type="submit"
-        classList={{
-          'absolute w-[100%] py-2 bottom-0 text-center': true,
-          'border-[0.3px] border-zinc-700/80 cursor-pointer hover:bg-zinc-700/20 rounded': true,
-        }}
-      >
-        Next
-      </button>
-    </form>
   );
 };
 
---
+++ packages/ui/src/pages/settings/export-key.tsx
@@ -0,0 +1,97 @@
+import { createEffect, createSignal, Match, Show, Switch, type Component } from 'solid-js';
+import { Wallet } from 'ethers';
+import { AuthStep } from './index';
+import { userStore } from '../../store';
+import { copyToClipboard } from '../../lib/utils';
+import { Link } from '../../components/link';
+import { ChevronLeftIcon } from '../../components/icons/chevron-left';
+import { CopyIcon } from '../../components/icons/copy';
+import { EyeIcon } from '../../components/icons/eye';
+import { EyeSlashIcon } from '../../components/icons/eye-slash';
+import { CheckIcon } from '../../components/icons/check';
+
+export const ExportKey: Component = () => {
+  const [currentStep, setCurrentStep] = createSignal<'authenticate' | 'reveal'>('authenticate');
+  return (
+    <div class="flex flex-col w-[360px] h-[540px] p-4 space-y-2 text-white bg-black border-[0.3px] border-zinc-700">
+      <Link path="/settings" class="flex items-center space-x-1 mb-4">
+        <ChevronLeftIcon />
+        <span>Settings / Export private key</span>
+      </Link>
+      <Switch>
+        <Match when={currentStep() === 'authenticate'}>
+          <AuthStep onNext={() => setCurrentStep(() => 'reveal')} />
+        </Match>
+        <Match when={currentStep() === 'reveal'}>
+          <RevealKeyStep />
+        </Match>
+      </Switch>
+    </div>
+  );
+};
+
+const RevealKeyStep: Component = () => {
+  const [copying, setCopying] = createSignal(false);
+  const [blurredOut, setBlurredOut] = createSignal(true);
+
+  createEffect(() => {
+    if (!copying()) return;
+    setTimeout(() => {
+      setCopying(false);
+    }, 2000);
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const wallet = Wallet.fromPhrase(userStore.mnemonic!);
+
+  const copyPrivateKey = () => {
+    setCopying(true);
+    copyToClipboard(wallet.privateKey);
+  };
+
+  return (
+    <div class="relative h-full space-y-4">
+      <div class="flex items-center justify-between">
+        <div class="uppercase">private key</div>
+        <Show
+          when={!copying()}
+          fallback={
+            <div class="flex items-center space-x-2 text-xs text-green-600">
+              <span>Copied</span>
+              <CheckIcon />
+            </div>
+          }
+        >
+          <button type="button" class="flex items-center space-x-2" onClick={copyPrivateKey}>
+            <span class="text-xs uppercase">copy</span>
+            <CopyIcon class="w-[12px] h-[12px]" />
+          </button>
+        </Show>
+      </div>
+      <div class="flex items-center justify-around p-2 border-[0.3px] border-zinc-700/80 rounded">
+        <p
+          classList={{
+            'w-[92%] text-sm select-none break-words line-clamp-3': true,
+            blur: blurredOut(),
+          }}
+        >
+          {wallet.privateKey}
+        </p>
+        <button type="button" onClick={() => setBlurredOut((prev) => !prev)}>
+          {blurredOut() ? <EyeIcon /> : <EyeSlashIcon />}
+        </button>
+      </div>
+      <p class="text-sm text-zinc-400">
+        <span class="font-bold">Never share it to anyone!</span> Your private key give you full
+        control on your wallet. If you lose your private key, you will lose access to your funds and
+        assets.
+      </p>
+      <Link
+        path="/"
+        class="absolute w-full py-2 bottom-0 text-center border-[0.3px] border-zinc-700/80 cursor-pointer hover:bg-zinc-700/20 rounded"
+      >
+        Close
+      </Link>
+    </div>
+  );
+};
